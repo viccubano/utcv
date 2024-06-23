e88aa447834478aeb4b683632c66c8952bdaa0e5
Victor Vega <isc.victor.vega@gmail.com>
2024-06-23 13:48:39 -0600

    spring

---
+++ src/test/java/com/utcv/team/TeamApplicationTests.java
@@ -0,0 +1,13 @@
+package com.utcv.team;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TeamApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
